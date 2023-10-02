9046121314247eb7d305e9fece32077fce7ac4b9
Yunus Altay <yunusaltay.nl@gmail.com>
2023-10-02 06:59:54 +0000

    instructions added

---
+++ commands.sh
@@ -0,0 +1,19 @@
+# # create a github repo
+# # clone the remote repo into the home directory
+# git clone https://github.com/Yunus-Altay/phonebook-heml-github-repo.git
+# cd phonebook-helm-github-repo/
+# create the chart package in the local git repo
+# helm package ../phonebookApp-chart/
+# helm repo index .
+# git add .
+# git config --global user.email "xxx"
+# git config --global user.name "xxx"
+# git commit -m "xxx"
+# git push
+# # github repo can be now used as helm repo
+---
+helm repo add phonebook https://raw.githubusercontent.com/Yunus-Altay/phonebook-helm-github-repo/main
+helm repo ls # see, the chart is added to the list
+helm install myapp phonebook/phonebook-app-chart
+# helm install myapp phonebook/phonebook-app-chart --set volumes.storage=8Gi
+# you may change the values of the helm chart as in the example above
